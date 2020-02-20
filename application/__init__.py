358653186e7737cc9c1852b519d00b140f10c052
bob-crutchley <bob.crutchley96@gmail.com>
2020-02-20 17:36:01 +0000

    added bcrypt

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
+bcrypt = Bcrypt(app)
 
 # config
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DB_URI'))
