db110b607ee25845233ec8d93e9e8e44c863dec2
bob-crutchley <bob.crutchley96@gmail.com>
2020-02-20 17:20:56 +0000

    csrf, added post form

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+
+# config
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DB_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 db = SQLAlchemy(app)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
-from application import app
+from flask import Flask, render_template, url_for, redirect
+from application import app, db
 from application.models import Post
+from application.forms import PostForm
 
 @app.route('/')
 @app.route('/home')
@@ -11,4 +12,19 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/post/create', methods=['GET', 'POST'])
+def post_create():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            title=form.title.data,
+            content=form.content.data
+        )
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("post.html", form=form)
 
